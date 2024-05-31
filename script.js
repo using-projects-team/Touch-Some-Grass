37e6e57e772c0372b703726abc4761d03a9c52be
Ernesto Gonzalez <ernestoale37@gmail.com>
2024-05-30 22:03:18 -0400

    Integrated Rylan's code

--- script.js
+++ script.js
@@ -1,36 +1,93 @@
 let autocomplete;
+let map, infoWindow;
+const pos = []
 var button = document.getElementById("Check");
 var favLocation = "";
+var daySel ="";
 // Rylan Maps API code [begins]
-function initAutocomplete() {
-    autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('autocomplete'),
-        {
-            types: ['(cities)'],
-            componentRestrictions: {'country': ['US']},
-            fields: ['place_id', 'geometry', 'name']
-        }
-    );
-    autocomplete.addListener('place_changed', placeChanged);
-}
-
 function initMap() {
     let orlando = {lat:28.5384, lng:-81.3789};
-    let map = new google.maps.Map(document.getElementById('map'), {zoom:8, center: orlando});
-    let marker = new google.maps.Marker({position: orlando, map: map});
+    let map = new google.maps.Map(
+        document.getElementById('map'), {zoom:10, center: orlando})
+    ;
+    
     initAutocomplete()
-}
 
-function placeChanged() {
-    var place = autocomplete.getPlace();
+    infoWindow = new google.maps.InfoWindow();
 
-    if (!place.geometry) {
-        document.getElementById('autocomplete').placeholder =
-        'Enter a City';
+
+  const locationButton = document.getElementById("locationButton");
+
+  locationButton.textContent = "Go to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  locationButton.addEventListener("click", () => {
+    
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+
+          console.log(pos)
+
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("You are here.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        },
+      );
     } else {
-        document.getElementById('details').innerHTML = place.name;
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
     }
+  });
 }
+
+
+console.log(pos)
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation.",
+  );
+  infoWindow.open(map);
+}
+
+        function initAutocomplete() {
+            autocomplete = new google.maps.places.Autocomplete(
+                document.getElementById('autocomplete'),
+                {
+                    types: ['(cities)'],
+                    componentRestrictions: {'country': ['US']},
+                    fields: ['place_id', 'geometry', 'name']
+
+                }
+            );
+            autocomplete.addListener('place_changed', placeChanged);
+        }
+        function placeChanged() {
+            var place = autocomplete.getPlace();
+
+            if (!place.geometry) {
+                document.getElementById('autocomplete').placeholder =
+                'Enter a City';
+            } else {
+                document.getElementById('details').innerHTML = place.name;
+            }
+        }
+
+window.initMap = initMap;
 // Rylan Maps API code [ends]
 // Prototype function for later, once maps API works properly and generates location strings.
 function populateArray() {
