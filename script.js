a1d8b7e514213830e9ad3eb0e38b8913b8a2b8a9
Ernesto Gonzalez <ernestoale37@gmail.com>
2024-06-01 14:28:31 -0400

    added button that redirects user to weather app if current location exists

--- script.js
+++ script.js
@@ -1,96 +1,81 @@
 let map, infoWindow;
 let autocomplete;
-
-
 const pos = []
 
 function initMap() {
     let orlando = {lat:28.5384, lng:-81.3789};
-    let map = new google.maps.Map(
-        document.getElementById('map'), {zoom:10, center: orlando})
-    ;
-    
-    initAutocomplete()
-
+    let map = new google.maps.Map(document.getElementById('map'), {zoom:10, center: orlando}); 
     infoWindow = new google.maps.InfoWindow();
-
-
-  const locationButton = document.getElementById("locationButton");
-
-  locationButton.textContent = "Go to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  locationButton.addEventListener("click", () => {
+    const locationButton = document.getElementById("locationButton");
+    locationButton.textContent = "Go to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    locationButton.addEventListener("click", () => {
     
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-
-          console.log(pos)
-          //Geolocation by name
-          var geoposition = new google.maps.Geocoder;
-          function geoLocation(coordinates) {
-            geoposition.geocode({'location': coordinates}, function(results, status) {
-              if (status === 'OK') {
-                if (results[0]) {
-                  document.getElementById("address").textContent = results[0].formatted_address;
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+  
+  
+            console.log(pos)
+            //Geolocation by name
+            var geoposition = new google.maps.Geocoder;
+            function geoLocation(coordinates) {
+              geoposition.geocode({'location': coordinates}, function(results, status) {
+                if (status === 'OK') {
+                  if (results[0]) {
+                    document.getElementById("address").textContent = results[0].formatted_address;
+                    document.getElementById("nextButton").removeAttribute("disabled");
+                  } else {
+                    document.getElementById("address").textContent = 'No results found';
+                  }
                 } else {
-                  document.getElementById("address").textContent = 'No results found';
+                  document.getElementById("address").textContent = 'Error detected';
                 }
-              } else {
-                document.getElementById("address").textContent = 'Error detected';
-              }
+              });
+            }
+            geoLocation(pos);
+            //end of geolocation function
+            //Find places nearby API, TODO: It is not going to work until we get a valid key
+            fetch('https://places.googleapis.com/v1/places:searchNearby', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'X-Goog-Api-Key': 'AIzaSyAks66r63NKJQCXN-ElReI33CP-EARiaTY',
+                'X-Goog-FieldMask': 'places.displayName'
+              },
+              body: JSON.stringify({
+                'includedTypes': ['historical_landmark', 'park', 'national_park', 'tourist_attraction', 'hiking_area'],
+                'maxResultCount': 1,
+                'rankPreference': 'DISTANCE',
+                'locationRestriction': {
+                  'circle': {
+                    'center': {
+                      'latitude': pos.lat,
+                      'longitude': pos.lng
+                    },
+                    'radius': 3000
+                  }
+                }
+              })
             });
-          }
-          geoLocation(pos);
-          //end of geolocation function
-          //Find places nearby API, TODO: It is not going to work until we get a valid key
-          fetch('https://places.googleapis.com/v1/places:searchNearby', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-Goog-Api-Key': 'AIzaSyAks66r63NKJQCXN-ElReI33CP-EARiaTY',
-              'X-Goog-FieldMask': 'places.displayName'
-            },
-            // body: '{\n  "maxResultCount": 10,\n  "rankPreference": "DISTANCE",\n  "locationRestriction": {\n    "circle": {\n      "center": {\n        "latitude": 37.7937,\n        "longitude": -122.3965\n      },\n      "radius": 1000.0\n    }\n  }\n}',
-            body: JSON.stringify({
-              'includedTypes': ['historical_landmark', 'park', 'national_park', 'tourist_attraction', 'hiking_area'],
-              'maxResultCount': 1,
-              'rankPreference': 'DISTANCE',
-              'locationRestriction': {
-                'circle': {
-                  'center': {
-                    'latitude': pos.lat,
-                    'longitude': pos.lng
-                  },
-                  'radius': 3000
-                }
-              }
-            })
-          });
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("You are here.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        },
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("You are here.");
+            infoWindow.open(map);
+            map.setCenter(pos);
+          },
+          () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+          },
+        );
+      } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    });
 }
-
-
-console.log(pos)
-
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
@@ -101,8 +86,8 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
-
-        function initAutocomplete() {
+// Scrapped idea that never worked
+/*       function initAutocomplete() {
             autocomplete = new google.maps.places.Autocomplete(
                 document.getElementById('autocomplete'),
                 {
@@ -123,6 +108,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
             } else {
                 document.getElementById('details').innerHTML = place.name;
             }
-        }
+        }*/
 
 window.initMap = initMap;
+
