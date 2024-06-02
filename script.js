dfd2f059f976ae746866374a85ec8f1f5d7ddb8e
Rylan Coglaiti <rylan.coglaiti@gmail.com>
2024-06-02 19:28:13 -0400

    Nearby Places

--- script.js
+++ script.js
@@ -1,19 +1,80 @@
-let map, infoWindow;
-let autocomplete;
-
-
-
-const pos = []
+var map, infoWindow;
+var autocomplete;
+var position = {};
+var service
 
 function initMap() {
-    let orlando = {lat:28.5384, lng:-81.3789};
-    let map = new google.maps.Map(
-        document.getElementById('map'), {zoom:10, center: orlando})
+  let orlando = { lat: 28.5384, lng: -81.3789 };
+  let map = new google.maps.Map(
+    document.getElementById('map'), { zoom: 15, center: orlando })
     ;
-    
-    initAutocomplete()
 
-    infoWindow = new google.maps.InfoWindow();
+  var input = document.getElementById('search');
+
+  let autocomplete = new google.maps.places.Autocomplete(input)
+
+  autocomplete.bindTo('bounds', map)
+
+  let marker = new google.maps.Marker({
+    map: map
+  })
+
+  google.maps.event.addListener(autocomplete, 'place_changed', () => {
+    var place = autocomplete.getPlace()
+    console.log(place)
+    console.log(place.photos[0].getUrl())
+
+
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport)
+    } else {
+      map.setCenter(place.geometry.location)
+      map.setZoom(16)
+    }
+    marker.setPosition(place.geometry.location)
+    marker.setVisible(true)
+
+    let request = {
+      location: place.geometry.location,
+      type: ['park', 'national_park', 'aquarium', 'hiking_area', 'zoo', 'historical_landmark', 'dog_park'],
+      radius: 8050
+    }
+
+    
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+
+
+    function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          var place = results[i];
+          createMarker(results[i]);
+        }
+      }
+
+
+      function createMarker(place) {
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
+
+        google.maps.event.addListener(marker, 'click', function () {
+          alert(place.name);
+          // window.open(place.photos[0].getUrl(), "_blank");
+          // infoWindow.open(map, this);
+        });
+
+      }
+    }
+
+
+  })
+
+
+  infoWindow = new google.maps.InfoWindow();
 
 
   const locationButton = document.getElementById("locationButton");
@@ -21,7 +82,7 @@ function initMap() {
   locationButton.textContent = "Go to Current Location";
   locationButton.classList.add("custom-map-control-button");
   locationButton.addEventListener("click", () => {
-    
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -31,6 +92,8 @@ function initMap() {
           };
 
 
+
+
           console.log(pos)
 
 
@@ -38,20 +101,21 @@ function initMap() {
           infoWindow.setContent("You are here.");
           infoWindow.open(map);
           map.setCenter(pos);
+          map.setZoom(13)
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
         },
+
       );
     } else {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
+
+  
   });
 }
-
-
-console.log(pos)
 
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -63,28 +127,5 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   );
   infoWindow.open(map);
 }
-
-        function initAutocomplete() {
-            autocomplete = new google.maps.places.Autocomplete(
-                document.getElementById('autocomplete'),
-                {
-                    types: ['(cities)'],
-                    componentRestrictions: {'country': ['US']},
-                    fields: ['place_id', 'geometry', 'name']
-
-                }
-            );
-            autocomplete.addListener('place_changed', placeChanged);
-        }
-        function placeChanged() {
-            var place = autocomplete.getPlace();
-
-            if (!place.geometry) {
-                document.getElementById('autocomplete').placeholder =
-                'Enter a City';
-            } else {
-                document.getElementById('details').innerHTML = place.name;
-            }
-        }
 
 window.initMap = initMap;
